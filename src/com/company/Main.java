f5e5537946e7f513948037f72f560e5008b19bff
sai1004 <saikiran.bavandla@gmail.com>
2020-11-28 18:53:02 +0530

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello Java");
+
+        System.out.println("88");
     }
 }
